8f203d91fee9e3ad70e92f0c1a68f188100a60d4
manisha yalla <abhishekyalla7@gmail.com>
2025-10-19 19:22:08 +0530

    new

--- new/jdf.sh
+++ new/jdf.sh
@@ -8,3 +8,22 @@ do
     sleep 1
     count=$((count + 1))  # OR: count=$((count + 1))
 done
+
+user-id=$(id -u )
+logfolder= var/log/shell-scrip
+logfile= $( echo $# ) | cut -d "*.log" -f1
+
+mkdir $logfile
+
+if [ user-id ]; then
+    echo " login with root acccess"
+    exit 1
+fi
+
+validate(){
+    if [ $# -le 0 ]; then
+        echo ""
+
+
+
+}
