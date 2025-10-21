b084ecdede7d2a0cb77fbfd82444085fd3688f10
manisha yalla <abhishekyalla7@gmail.com>
2025-10-21 19:28:23 +0530

    new

---
+++ new/practice.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+file=$1
+if [ $# -ne 1]; then
+
+ echo "pass argunents"
+ exit 1
+ fi
+
+ if [ -r $1]; then
+    echo "read permission is there"
+     else
+    
+  fi
+ if [ -w $1]; then
+    echo "write permission is there"
+     else
+    
+  fi
